fe129d8968f6170f584f449b7e45a6b671058b4d
JoanKenedy <joankenedi9311@gmail.com>
2024-02-27 19:20:46 -0600

    menu-menu

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-
+  collapsed = false;
+  onMenu() {
+    this.collapsed = !this.collapsed;
+  }
 }
